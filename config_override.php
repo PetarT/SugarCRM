6b586d12d883a115509b1e17b8869da6d6e74a03
Petar Tuovic <pelesrb@gmail.com>
2018-04-10 16:11:35 +0200

    Init commit

---
+++ config_override.php
@@ -0,0 +1,5 @@
+<?php
+/***CONFIGURATOR***/
+$sugar_config['default_date_format'] = 'd-m-Y';
+$sugar_config['default_time_format'] = 'H:i';
+/***CONFIGURATOR***/
---
+++ custom/blowfish/BhgObhaqRznvy.php
@@ -0,0 +1,5 @@
+<?php
+// created: 2018-04-10 00:20:48
+$key = array (
+  0 => 'b69d2c5f-67a3-aa1d-f405-5acbe7d6a920',
+);

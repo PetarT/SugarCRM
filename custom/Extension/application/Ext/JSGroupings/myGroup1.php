c71fc1ff04a7bd32406e008b37e118a44f63df84
Petar Tuovic <pelesrb@gmail.com>
2018-04-11 15:58:25 +0200

    Adding jsgroupings example

---
+++ custom/Extension/application/Ext/JSGroupings/myGroup1.php
@@ -0,0 +1,5 @@
+<?php
+$js_groupings[] = $newGrouping = array(
+    'custom/js/first.js'  => 'include/javascript/myGroup1.js',
+    'custom/js/second.js' => 'include/javascript/myGroup1.js',
+);
---
+++ custom/application/Ext/JSGroupings/jsgroups.ext.php
@@ -0,0 +1,10 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+$js_groupings[] = $newGrouping = array(
+    'custom/js/first.js'  => 'include/javascript/myGroup1.js',
+    'custom/js/second.js' => 'include/javascript/myGroup1.js',
+);
+
+?>
